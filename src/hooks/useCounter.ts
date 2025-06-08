5b2ecffb8a307cff819427215e219ce0e04c61cb
Fredy <macacoloco696@gmail.com>
2025-06-08 00:17:53 -0600

    useCounter custom hook

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import {gsap} from 'gsap';
 
+
 const MAXIMUN_COUNT = 10;
 
-export const CounterEffect = () => {
 
+export const useCounter = () => {
     const [counter, setCounter] = useState(5)
 
     const counterElement = useRef<HTMLHeadingElement>(null);
@@ -17,24 +18,18 @@ export const CounterEffect = () => {
     
         if(counter < 10) return;
 
-        //console.log('%cse llego al valor maximo', 'color:red')
+        console.log('%cse llego al valor maximo', 'color:red')
 
         const tl = gsap.timeline();
 
         tl.to(counterElement.current,{y:-10, duration: 0.2, ease: 'ease.out'})
           .to(counterElement.current,{y:0, duration: 1, ease: 'bounce.out'})
 
-       
-
     }, [counter])
 
-    return (
-        <>
-          <h1>CounterEffect:</h1>
-          <h2 ref={counterElement} >{counter}</h2>
-            <button onClick={handleClick}>
-                +1
-            </button> 
-        </>
-    )
+    return {
+        counter,
+        counterElement,
+        handleClick
+    }
 }
