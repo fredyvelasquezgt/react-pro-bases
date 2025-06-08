b5b19a8b3c0faa0f8f0cd734cb8e99d50ff85437
Fredy <macacoloco696@gmail.com>
2025-06-08 16:03:41 -0600

    last changes useCounter

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,35 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {gsap} from 'gsap';
 
 
-const MAXIMUN_COUNT = 10;
 
-
-export const useCounter = () => {
+export const useCounter = ({maxCount = 10}) => {
     const [counter, setCounter] = useState(5)
 
-    const counterElement = useRef<HTMLHeadingElement>(null);
+    const elementToAnimate = useRef<any>(null);
+
+    const tl = useRef(gsap.timeline())
 
     const handleClick = () => {
-        setCounter(prev => Math.min(prev + 1, MAXIMUN_COUNT));
+        setCounter(prev => Math.min(prev + 1, maxCount));
     }
 
+    useLayoutEffect(() => {
+
+        if(!elementToAnimate.current) return
+
+        tl.current.to(elementToAnimate.current,{y:-10, duration: 0.2, ease: 'ease.out'})
+          .to(elementToAnimate.current,{y:0, duration: 1, ease: 'bounce.out'})
+          .pause()
+    }, [])
+
     useEffect(() => {
-    
-        if(counter < 10) return;
+        
+        //if(counter < maxCount) return;
 
-        console.log('%cse llego al valor maximo', 'color:red')
-
-        const tl = gsap.timeline();
-
-        tl.to(counterElement.current,{y:-10, duration: 0.2, ease: 'ease.out'})
-          .to(counterElement.current,{y:0, duration: 1, ease: 'bounce.out'})
-
+        tl.current.play(0)
+       
     }, [counter])
 
     return {
         counter,
-        counterElement,
+        elementToAnimate,
         handleClick
     }
 }
